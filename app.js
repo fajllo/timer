98a558399721d46f5e3f3932855b0b910a0c524f
fsdr <filipsordyl8@unilever.com>
2020-08-24 22:22:17 +0200

    creating input and 2 btn and select them in js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+//class time 
+// start()
+//stop()
+//durationChange
+//tick()
+class Timer {
+    constructor(durationInput,startBtn,stopBtn){
+        this.durationInput = durationInput;
+        this.startBtn = startBtn;
+        this.stopBtn = stopBtn;
 
+        this.startBtn.addEventListener("click",this.start)
+        
+    }
+    start(){
+        console.log("count down has began")
+    }
+}
+const startBtn = document.querySelector("#start")
+const stopBtn = document.querySelector("#stop")
+const stopBtn = document.querySelector("#duration")
+
+const timer = new Timer(0,startBtn,stopBtn);
