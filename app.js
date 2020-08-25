3b1b46e4699168e69e9de47e61470d8be18e8ecf
fsdr <filipsordyl8@unilever.com>
2020-08-25 21:53:10 +0200

    add callback to class

--- app.js
+++ app.js
@@ -4,16 +4,24 @@
 //durationChange
 //tick()
 class Timer {
-    constructor(durationInput = 0,startBtn,stopBtn){
+    constructor(durationInput = 0,startBtn,stopBtn, callback){
         this.durationInput = durationInput;
         this.startBtn = startBtn;
         this.stopBtn = stopBtn;
+       if (callback){
+           this.onStart = callback.onStart;
+           this.onTick = callback.onTick;
+           this.onComplete = callback.onComplete;
+       }
 
         this.startBtn.addEventListener("click",this.start)
         this.stopBtn.addEventListener("click",this.stop)
         
     }
     start = () =>{
+        if (this.onStart){
+            this.onStart();
+        }
         this.tick();
         this.counter = setInterval(this.tick,1000);
         
@@ -21,13 +29,24 @@ class Timer {
     }
     stop = () => {
         clearInterval(this.counter);
+        const h1 = document.createElement("h1");
+        h1.innerText = "koniec";
+        document.body.appendChild(h1);
+
     }
     tick = () => {
+        console.log(this.durationInput.value)
         if (this.timeLeft <= 0 ){
             this.stop();
+            if (this.onComplete){
+                this.onComplete();
+            }
         }
         else{
             this.timeLeft = this.timeLeft -1;
+            if (this.onTick){
+                this.onTick();
+            }
         }
 
     }
@@ -37,12 +56,24 @@ class Timer {
     }
 
     set timeLeft(time){
-        this.durationInput.value =time
+        this.durationInput.value = time
 
     }
+
 }
 const startBtn = document.querySelector("#start")
 const stopBtn = document.querySelector("#stop")
 const duration = document.querySelector("#duration")
 
-const timer = new Timer(duration,startBtn,stopBtn);
+const timer = new Timer(duration,startBtn,stopBtn, {
+    onStart(){
+        console.log("start")
+    },
+    onTick(){
+        console.log("-1")
+    },
+    onComplete(){
+        console.log("complete")
+
+    }
+});
